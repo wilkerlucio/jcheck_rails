ff88f14c9f5f329742bd3b38c057fe344763dc1b
Wilker Lucio <wilkerlucio@gmail.com>
2010-08-12 01:21:06 -0300

    added encoders

---
+++ lib/jcheck_rails/encoder.rb
@@ -0,0 +1,43 @@
+require 'action_view'
+
+module JcheckRails
+  module Encoder
+    class << self
+      include ActionView::Helpers::JavaScriptHelper
+      
+      def convert_to_javascript(object)
+        return "true" if object == {}
+
+        case object
+          when Symbol, String
+            "\"#{escape_javascript object.to_s}\""
+          when Regexp
+            "/#{object.source}/#{regex_options_string(object.options)}"
+          when Hash
+            convert_hash(object)
+          else
+            raise ArgumentError.new("can't parse object type #{object.class}")
+        end
+      end
+
+      def convert_hash(hash)
+        hash_pairs = []
+
+        hash.each do |k, v|
+          hash_pairs << "#{convert_to_javascript(k)}: #{convert_to_javascript(v)}"
+        end
+
+        "{#{hash_pairs.join(", ")}}"
+      end
+
+      def regex_options_string(options)
+        letters = [[1, "i"], [2, "x"], [4, "m"]]
+
+        letters.inject("") do |acc, (v, l)|
+          acc << l if (options & v) > 0
+          acc
+        end
+      end
+    end
+  end
+end
---
+++ spec/encoder_spec.rb
@@ -0,0 +1,45 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "JcheckRails Encoder" do
+  context "parsing regexp options" do
+    it "should see ignore case option" do
+      JcheckRails::Encoder.regex_options_string(/a/i.options).should == "i"
+    end
+    
+    it "should see extended option" do
+      JcheckRails::Encoder.regex_options_string(/a/x.options).should == "x"
+    end
+    
+    it "should see multiline option" do
+      JcheckRails::Encoder.regex_options_string(/a/m.options).should == "m"
+    end
+    
+    it "should work with combined options" do
+      JcheckRails::Encoder.regex_options_string(/a/im.options).should == "im"
+    end
+  end
+  
+  context "parsing hashes" do
+    it "convert hash keys and values" do
+      JcheckRails::Encoder.convert_hash({:key => "value", "other" => "thing"}).should == '{"key": "value", "other": "thing"}'
+    end
+  end
+  
+  context "converting values" do
+    it "should convert blank hashes to true" do
+      JcheckRails::Encoder.convert_to_javascript({}).should == "true"
+    end
+    
+    it "should generate strings with single quotes" do
+      JcheckRails::Encoder.convert_to_javascript("some string").should == '"some string"'
+    end
+    
+    it "should generate strings with escaped quotes" do
+      JcheckRails::Encoder.convert_to_javascript('some "quote"').should == '"some \\"quote\\""'
+    end
+    
+    it "should convert regular expressions" do
+      JcheckRails::Encoder.convert_to_javascript(/^[sS]ome_reg/im).should == "/^[sS]ome_reg/im"
+    end
+  end
+end
