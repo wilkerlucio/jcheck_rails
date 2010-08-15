afc4c8148bc9e6647d0ace34980dfe6b767593dd
Wilker Lucio <wilkerlucio@gmail.com>
2010-08-15 14:29:54 -0300

    configure rspec for focus metadata

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'jcheck_rails'
 require 'rspec'
 
+RSpec.configure do |c|
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
+end
+
 require 'active_model'
 
 def jcheck(*args)
