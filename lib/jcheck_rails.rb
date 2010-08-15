d933b2ad1c92694f6c07e5e9b579968edf4253b8
Wilker Lucio <wilkerlucio@gmail.com>
2010-08-15 14:30:16 -0300

    added more validators support

--- lib/jcheck_rails.rb
+++ lib/jcheck_rails.rb
@@ -2,20 +2,43 @@ require 'jcheck_rails/encoder'
 
 module JcheckRails
   class << self
-    def jcheck_for(object)
+    def jcheck_for(object, attribute = nil)
+      return jcheck_for_object_attribute(object, attribute) if attribute
+      
       validations = []
       
       object.class._validators.each do |attribute, validators|
-        attr_validations = []
-
-        validators.each do |validator|
-          attr_validations << "#{Encoder.convert_to_javascript validator.kind}: #{Encoder.convert_to_javascript(validator.options)}"
-        end
+        attr_validations = jcheck_for_object_attribute(object, attribute)
         
-        validations << "validator.validates(#{Encoder.convert_to_javascript attribute}, {#{attr_validations.join(", ")}});"
+        validations << "validator.validates(#{Encoder.convert_to_javascript attribute}, #{attr_validations.join(", ")});"
       end
       
       %{<script type="text/javascript"> var validator = $("form").jcheck(); #{validations.join(" ")} </script>}
+    end
+    
+    def jcheck_for_object_attribute(object, attribute)
+      validations = object.class._validators[attribute].inject([]) do |acc, validator|
+        options = filter_validator_options(validator)
+        
+        acc << "#{Encoder.convert_to_javascript validator.kind}: #{Encoder.convert_to_javascript(options)}"
+      end
+      
+      "{#{validations.join(', ')}}"
+    end
+    
+    protected
+    
+    def filter_validator_options(validator)
+      options = validator.options.dup
+      
+      case validator.kind
+      when :acceptance
+        options.delete :allow_nil
+      when :length
+        options.delete :tokenizer
+      end
+      
+      options
     end
   end
 end
--- spec/jcheck_rails_spec.rb
+++ spec/jcheck_rails_spec.rb
@@ -2,6 +2,60 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "JcheckRails" do
   context "rails validators" do
+    it "should return blank object if attribute has no validations" do
+      m = mock_model do
+        attr_accessor :name
+      end
+      
+      jcheck(m, :name).should == "{}"
+    end
+    
+    context "acceptance validator" do
+      it "should generate jcheck validator" do
+        m = mock_model do
+          attr_accessor :terms
+          
+          validates_acceptance_of :terms
+        end
+        
+        jcheck(m, :terms).should == "{'acceptance': {'accept': '1'}}"
+      end
+      
+      it "should accept a custom acceptance" do
+        m = mock_model do
+          attr_accessor :terms
+          
+          validates_acceptance_of :terms, :accept => "3"
+        end
+        
+        jcheck(m, :terms).should == "{'acceptance': {'accept': '3'}}"
+      end
+    end
+    
+    context "confirmation" do
+      it "should generate jcheck validator" do
+        m = mock_model do
+          attr_accessor :password, :password_confirmation
+          
+          validates_confirmation_of :password
+        end
+        
+        jcheck(m, :password).should == "{'confirmation': true}"
+      end
+    end
+    
+    context "exclusion" do
+      it "should generate jcheck validator" do
+        m = mock_model do
+          attr_accessor :login
+          
+          validates_exclusion_of :login, :in => ["admin", "bot"]
+        end
+        
+        jcheck(m, :login).should == "{'exclusion': {'in': ['admin', 'bot']}}"
+      end
+    end
+    
     context "format validator" do
       it "should generate jcheck validator" do
         m = mock_model do
@@ -9,7 +63,7 @@ describe "JcheckRails" do
           validates_format_of :name, :with => /^[a-z]regex$/i
         end
         
-        jcheck(m).should include("validator.validates('name', {'format': {'with': /^[a-z]regex$/i}});")
+        jcheck(m, :name).should == "{'format': {'with': /^[a-z]regex$/i}}"
       end
       
       it "should work with 'without' option" do
@@ -18,8 +72,66 @@ describe "JcheckRails" do
           validates_format_of :name, :without => /^[a-z]regex$/i
         end
         
-        jcheck(m).should include("validator.validates('name', {'format': {'without': /^[a-z]regex$/i}});")
+        jcheck(m, :name).should == "{'format': {'without': /^[a-z]regex$/i}}"
       end
+    end
+    
+    context "inclusion validator" do
+      it "should generate jcheck validator" do
+        m = mock_model do
+          attr_accessor :civil_state
+          
+          validates_inclusion_of :civil_state, :in => ["single", "married"]
+        end
+        
+        jcheck(m, :civil_state).should == "{'inclusion': {'in': ['single', 'married']}}"
+      end
+    end
+    
+    context "length validator" do
+      it "should generate jcheck with is" do
+        m = mock_model do
+          attr_accessor :message
+          
+          validates_length_of :message, :is => 10
+        end
+        
+        jcheck(m, :message).should == "{'length': {'is': 10}}"
+      end
+      
+      it "should generate jcheck with minimum" do
+        m = mock_model do
+          attr_accessor :message
+          
+          validates_length_of :message, :minimum => 10
+        end
+        
+        jcheck(m, :message).should == "{'length': {'minimum': 10}}"
+      end
+      
+      it "should generate jcheck with maximum" do
+        m = mock_model do
+          attr_accessor :message
+          
+          validates_length_of :message, :maximum => 10
+        end
+        
+        jcheck(m, :message).should == "{'length': {'maximum': 10}}"
+      end
+      
+      it "should generate jcheck with minimum and maximum" do
+        m = mock_model do
+          attr_accessor :message
+          
+          validates_length_of :message, :minimum => 2, :maximum => 10
+        end
+        
+        jcheck(m, :message).should == "{'length': {'minimum': 2, 'maximum': 10}}"
+      end
+    end
+    
+    context "numericality validator" do
+      
     end
     
     context "presence validator" do
@@ -29,7 +141,7 @@ describe "JcheckRails" do
           validates_presence_of :name
         end
         
-        jcheck(m).should include("validator.validates('name', {'presence': true});")
+        jcheck(m, :name).should == "{'presence': true}"
       end
     end
   end
